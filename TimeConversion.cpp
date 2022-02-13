851b0cd09cef4ed36193e8fe0cad4438d90de640
TRECN <rishabh1103@outlook.com>
2022-02-13 19:00:20 +0530

    commit

---
+++ TimeConversion.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    string h,noon;
+    h=s.substr(0,2);
+    int hour=stoi(h);
+    noon=s.substr(8,10);
+    return noon;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    cout<<result;
+    return 0;
+}
