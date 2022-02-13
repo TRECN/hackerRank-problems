dda3868536e4718ba353a340db61594608e64eb2
TRECN <rishabh1103@outlook.com>
2022-02-13 18:27:34 +0530

    commit

--- digonalDiff.cpp
+++ digonalDiff.cpp
@@ -115,7 +115,18 @@ int main()
 {
     ofstream fout(getenv("OUTPUT_PATH"));
 
-n, arr_row_temp_temp);
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
 
         vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
 
