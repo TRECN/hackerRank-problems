d38bc6ad100add090b3f287897feaf13830ea5c8
TRECN <rishabh1103@outlook.com>
2022-02-13 19:15:14 +0530

    commit

--- TimeConversion.cpp
+++ TimeConversion.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
-ime=h+s.substr(2,6);
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    string h,noon;
+    h=s.substr(0,2);
+    int hour=stoi(h);
+    noon=s.substr(8,10);
+    if(noon=="AM"&&hour==12)
+        h="00";
+    if(noon=="PM"&&hour!=12){
+        hour+=12;
+        h="";
+        h=to_string(hour);
+    }
+    string time=h+s.substr(2,6);
     return time;
 }
 
