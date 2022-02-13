62158a639644e5455afe7fcd15f22c1e1ba352c4
TRECN <rishabh1103@outlook.com>
2022-02-13 19:15:26 +0530

    commit

--- TimeConversion.cpp
+++ TimeConversion.cpp
@@ -25,6 +25,12 @@ string timeConversion(string s) {
     return time;
 }
 
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
 
     string result = timeConversion(s);
 
