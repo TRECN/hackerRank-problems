5d384d0fde285a6aeac13ae0b3d2ad8bf7c2cc7b
TRECN <rishabh1103@outlook.com>
2022-02-13 18:27:17 +0530

    commit

--- plus Minus.cpp
+++ plus Minus.cpp
@@ -71,23 +71,6 @@ void plusMinus(vector<int> arr) {
 
 int main()
 {
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
-    }
 
     plusMinus(arr);
 
