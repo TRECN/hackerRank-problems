296f0d370f3bf7a27d38073d93027f08d5a671b1
TRECN <rishabh1103@outlook.com>
2022-02-14 21:52:45 +0530

    commit

--- TimeConversion.cpp
+++ TimeConversion.cpp
@@ -14,7 +14,13 @@ string timeConversion(string s) {
     h=s.substr(0,2);
     int hour=stoi(h);
     noon=s.substr(8,10);
-   
+    if(noon=="AM"&&hour==12)
+        h="00";
+    if(noon=="PM"&&hour!=12){
+        hour+=12;
+        h="";
+        h=to_string(hour);
+    }
     string time=h+s.substr(2,6);
     return time;
 }
