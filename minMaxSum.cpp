a0adfe56b86efe038fd6fe1e0f3c591fbe708965
TRECN <rishabh1103@outlook.com>
2022-02-14 22:05:58 +0530

    commit

--- minMaxSum.cpp
+++ minMaxSum.cpp
@@ -1,10 +1,4 @@
-// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
-
-// Example
-
-// The minimum sum is  and the maximum sum is . The function prints
-
-// 16 24
+4
 // Function Description
 
 // Complete the miniMaxSum function in the editor below.
