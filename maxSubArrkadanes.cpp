e58cde6653292a7b0e82efcb77196444aed6e683
TRECN <rishabh1103@outlook.com>
2022-02-16 23:40:28 +0530

    commit

--- maxSubArrkadanes.cpp
+++ maxSubArrkadanes.cpp
@@ -1,4 +1,15 @@
 #include <bits/stdc++.h>
-
+using namespace std;
+int main(){
+   int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   int ans=INT_MIN;
+   int sum=0;
+   for(int i=0;i<n;i++){
+        sum+=arr[i];
+        ans=max(sum,ans);
+        if(sum<0)
+            sum=0;
+   }
    cout << "Maximum Sum Contiguous Subarray = "<< ans;
 }
