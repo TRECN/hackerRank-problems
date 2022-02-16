05b590b5d80efa1853ff26bed9cb2d0dc71cc244
TRECN <rishabh1103@outlook.com>
2022-02-16 23:30:00 +0530

    commit

--- RichestCustomerWealth.cpp
+++ RichestCustomerWealth.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        
+        vector<int>money;
+        for(int i=0;i<accounts.size();i++){
+            for(int j=0;j<accounts[0].size();j++){
+                
+            }
+        }
     }
 };
