e446a3cd293841a11dbadd506a6b8cf7ae77630b
TRECN <rishabh1103@outlook.com>
2022-02-16 20:05:54 +0530

    commit

--- BTimeToBStock.cpp
+++ BTimeToBStock.cpp
@@ -5,9 +5,11 @@ public:
         for(int i=0;i<prices.size()-1;i++){
             for(int j=i+1;j<prices.size();i++){
                 
-                if(dif<abs(prices[i]-prices[j]))
-                    
+                if(dif<abs(prices[i]-prices[j])){
+                    dif=abs(prices[i]-prices[j]);
+                }
             }
         }
+        return dif;
     }
 };
