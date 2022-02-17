4f2f228ce8a683848b3ac5690f5accba0db4a286
TRECN <rishabh1103@outlook.com>
2022-02-17 18:51:33 +0530

    commit

---
+++ reshape.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        vector<int>mat1;
+        for (int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+
+            }
+        }
+    }
+};
