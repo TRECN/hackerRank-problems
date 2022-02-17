92aafc367898ded756334b5a8dfbf6373e4d979e
TRECN <rishabh1103@outlook.com>
2022-02-17 21:57:11 +0530

    commit

--- reshape.cpp
+++ reshape.cpp
@@ -8,10 +8,10 @@ public:
             }
         }
         vector<vector<int>>result(r,vector<int>(c));
-
-        for (int i=0,k=0;i<r;i++){
+        int k=0;
+        for (int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                result[i].push_back(mat1[k]);
+                result[i][j]=mat1[k];
                 k++;
             }
         }
