e68a1e3671bf53d11d5c33cc464f79cb8a5ce6ff
TRECN <rishabh1103@outlook.com>
2022-02-18 21:04:28 +0530

    commit

--- GradingStudent.cpp
+++ GradingStudent.cpp
@@ -36,32 +36,7 @@ int main()
     vector<int> grades(grades_count);
 
     for (int i = 0; i < grades_count; i++) {
-        string grades_item_temp;
-        getline(cin, grades_item_temp);
 
-        int grades_item = stoi(ltrim(rtrim(grades_item_temp)));
-
-        grades[i] = grades_item;
-    }
-
-    vector<int> result = gradingStudents(grades);
-
-    for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << "\n";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
     string s(str);
 
     s.erase(
