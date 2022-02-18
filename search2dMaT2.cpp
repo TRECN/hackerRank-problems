6dc51161b2e96c52748ca92d7841261d480a08f6
TRECN <rishabh1103@outlook.com>
2022-02-18 17:10:51 +0530

    commit

--- search2dMaT2.cpp
+++ search2dMaT2.cpp
@@ -8,17 +8,17 @@ public:
             Mtar=matrix[i];
             int l=0;
             int r=Mtar.size()-1;
-                while(l<=r){
-            int m=l+(r-l)/2;
+            while(l<=r){
+                int m=l+(r-l)/2;
 
-            if(Mtar[m]==target)
-                return true;
+                if(Mtar[m]==target)
+                    return true;
 
-            if(Mtar[m]<target)
-                l=m+1;
-            else    
-                r=m-1;
-        }
+                if(Mtar[m]<target)
+                    l=m+1;
+                else    
+                    r=m-1;
+            }
         }
         return false;
         
