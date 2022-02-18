157a449512f0dc65f594ab31255c2778f73fabb1
TRECN <rishabh1103@outlook.com>
2022-02-18 21:02:22 +0530

    commit

--- subarraydiv.cpp
+++ subarraydiv.cpp
@@ -3,6 +3,15 @@ int birthday(vector<int> s, int d, int m) {
     if(m>s.size())
         return 0;
     else{
-
+        for(int i=0;i<s.size()&&(i+m)<=s.size();i++){
+            int sum=0;
+            for(int j=0,k=i;j<m;j++,k++){
+                sum+=s[k];
+            }
+            if(sum==d){
+                count++;
+            }
+        }
+    }
     return count;
 }
