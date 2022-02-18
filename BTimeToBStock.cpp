4eed2b2ce73a4a17685175cae840c5f17e7c0b96
TRECN <rishabh1103@outlook.com>
2022-02-18 21:03:19 +0530

    commit

--- BTimeToBStock.cpp
+++ BTimeToBStock.cpp
@@ -1,4 +1,13 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int dif=0;
+        int min=INT_MAX,max=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }
+            max=prices[i]-min;
             if(dif<max){
                 dif=max;
             }
