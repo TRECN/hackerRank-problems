f54f282e1446b80b533b70eeac57b6b465191c3c
TRECN <rishabh1103@outlook.com>
2022-02-18 19:33:37 +0530

    commit

--- breakingTheRecords.cpp
+++ breakingTheRecords.cpp
@@ -3,7 +3,19 @@ vector<int> breakingRecords(vector<int> scores) {
     int min=scores[0];
     int maxC=0;
     int minC=0;
+
     for(int i=0;i<scores.size();i++){
-        
+        if(max<scores[i]){
+            maxC++;
+            max=scores[i];
+        }
+        if(min>scores[i]){
+            minC++;
+            min=scores[i];
+        }
     }
+    vactor <int> result;
+    result.push_back(maxC);
+    result.push_back(minC);
+    return result;
 }
