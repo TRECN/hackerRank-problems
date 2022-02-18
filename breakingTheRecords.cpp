c392018865474903bbb3fe76359247c8a390bc2d
TRECN <rishabh1103@outlook.com>
2022-02-18 19:30:19 +0530

    commit

---
+++ breakingTheRecords.cpp
@@ -0,0 +1,9 @@
+vector<int> breakingRecords(vector<int> scores) {
+    int max=scores[0];
+    int min=scores[0];
+    int maxC=0;
+    int minC=0;
+    for(int i=0;i<scores.size();i++){
+        
+    }
+}
