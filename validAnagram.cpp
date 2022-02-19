786c63f37bff99258807ca877643d3473002252e
TRECN <rishabh1103@outlook.com>
2022-02-19 19:10:16 +0530

    commit

--- validAnagram.cpp
+++ validAnagram.cpp
@@ -4,5 +4,19 @@ public:
         map<char,int>s1;
         map<char,int>t1;
         
-
+        for(auto ch: s){
+            s1[ch]++;
+        }
+        for(auto ch: t){
+            t1[ch]++;
+        }
+        if(s.length()!=t.length())
+            return false;
+        for(auto ch: s){
+            if(s1[ch]!=t1[ch])
+                return false;
+        }
+        return true;
+        
+    }
 };
