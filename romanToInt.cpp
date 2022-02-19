32200a44547380af0001f07f47fa371bda24c7ce
TRECN <rishabh1103@outlook.com>
2022-02-19 19:09:15 +0530

    commit

--- romanToInt.cpp
+++ romanToInt.cpp
@@ -11,24 +11,7 @@ public:
         rom.insert({'D',500});
         rom.insert({'M',1000});
         int ans=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='V' && i>0 && s[i-1]=='I'){
-                ans+=4;
-                --i;
-            }
-            else if(s[i]=='X' && i>0 && s[i-1]=='I'){
-                ans+=9;
-                --i;
-            }
-            else if(s[i]=='L' && i>0 && s[i-1]=='X'){
-                ans+=40;
-                --i;
-            }
-            
-            else if(s[i]=='C' && i>0 && s[i-1]=='X'){
-                ans+=90;
-                --i;
-            }
+
             
             else if(s[i]=='D' && i>0 && s[i-1]=='C'){
                 ans+=400;
