26a7388ccacc92019c29401ac007f671aa425141
TRECN <rishabh1103@outlook.com>
2022-02-19 19:11:27 +0530

    commit

--- GradingStudent.cpp
+++ GradingStudent.cpp
@@ -5,7 +5,24 @@ using namespace std;
 string ltrim(const string &);
 string rtrim(const string &);
 
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
 
+vector<int> gradingStudents(vector<int> grades) {
+    for(int i=0;i<grades.size();i++){
+        if(grades[i]>=38){
+            if(grades[i]%5==3)
+            grades[i]+=2;
+        if(grades[i]%5==4)
+            grades[i]+=1;
+        }
+    }
+    return grades;
+}
 
 int main()
 {
