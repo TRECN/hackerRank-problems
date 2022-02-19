1355758d9cf61d35797f9429b2dfaba249349ce0
TRECN <rishabh1103@outlook.com>
2022-02-19 19:10:27 +0530

    commit

--- RansomNote.cpp
+++ RansomNote.cpp
@@ -1,3 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int>ranMap;
+        map<char, int>magMap;
+        for(auto ch:ransomNote)
+            ranMap[ch]++;
+            
+        for(auto ch:magazine)
+            magMap[ch]++;
 
         for(auto ch:ransomNote)
            {
