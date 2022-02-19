bf65f69c2566f2651de1b00e30cca2e67748a5ac
TRECN <rishabh1103@outlook.com>
2022-02-19 19:10:00 +0530

    commit

--- romanToInt.cpp
+++ romanToInt.cpp
@@ -16,24 +16,7 @@ public:
                 ans+=4;
                 --i;
             }
-            else if(s[i]=='X' && i>0 && s[i-1]=='I'){
-                ans+=9;
-                --i;
-            }
-            else if(s[i]=='L' && i>0 && s[i-1]=='X'){
-                ans+=40;
-                --i;
-            }
-            
-            else if(s[i]=='C' && i>0 && s[i-1]=='X'){
-                ans+=90;
-                --i;
-            }
-            
-            else if(s[i]=='D' && i>0 && s[i-1]=='C'){
-                ans+=400;
-                --i;
-            }
+    
             
             else if(s[i]=='M' && i>0 && s[i-1]=='C'){
                 ans+=900;
