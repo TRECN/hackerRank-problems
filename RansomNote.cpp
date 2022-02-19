1f357bf593d22e0d0f7cf11fba08e3057d2093bb
TRECN <rishabh1103@outlook.com>
2022-02-19 18:10:04 +0530

    commit

--- RansomNote.cpp
+++ RansomNote.cpp
@@ -5,15 +5,15 @@ public:
         int m=magazine.length();
         if(r<m){
             for(int i=0;i<m&&(i+r)<=m;i++){
-                if(ransomeNote==magazine.substr(i,r)){
+                if(ransomNote==magazine.substr(i,r)){
                     return true;
                 }
             }
 
         }
-        else if(r==m&&ransomeNote==magazine){
+        else if(r==m&&ransomNote==magazine){
             return true;
         }
-        return false
+        return false;
     }
 };
