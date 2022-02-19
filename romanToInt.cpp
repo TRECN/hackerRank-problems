88fefe3f827671d5722b7fa89b2a9b8df76c5889
TRECN <rishabh1103@outlook.com>
2022-02-19 19:09:47 +0530

    commit

--- romanToInt.cpp
+++ romanToInt.cpp
@@ -3,23 +3,7 @@ public:
     int romanToInt(string s) {
         int n=s.length();
         unordered_map<char,int>rom;
-        rom.insert({'I',1});
-        rom.insert({'V',5});
-        rom.insert({'X',10});
-        rom.insert({'L',50});
-        rom.insert({'C',100});
-        rom.insert({'D',500});
-        rom.insert({'M',1000});
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='V' && i>0 && s[i-1]=='I'){
-                ans+=4;
-                --i;
-            }
-            else if(s[i]=='X' && i>0 && s[i-1]=='I'){
-                ans+=9;
-                --i;
-            }
+
             else if(s[i]=='L' && i>0 && s[i-1]=='X'){
                 ans+=40;
                 --i;
