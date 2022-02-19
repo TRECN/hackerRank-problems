da48ec31ab870e7222cd4b3e6823fa05594ecbfe
TRECN <rishabh1103@outlook.com>
2022-02-19 19:09:36 +0530

    commit

--- romanToInt.cpp
+++ romanToInt.cpp
@@ -21,27 +21,6 @@ public:
                 --i;
             }
             else if(s[i]=='L' && i>0 && s[i-1]=='X'){
-                ans+=40;
-                --i;
-            }
-            
-            else if(s[i]=='C' && i>0 && s[i-1]=='X'){
-                ans+=90;
-                --i;
-            }
-            
-            else if(s[i]=='D' && i>0 && s[i-1]=='C'){
-                ans+=400;
-                --i;
-            }
-            
-            else if(s[i]=='M' && i>0 && s[i-1]=='C'){
-                ans+=900;
-                --i;
-            }
-            else    
-                ans+=rom[s[i]];
-        }
-        return ans;
+
     }
 };
