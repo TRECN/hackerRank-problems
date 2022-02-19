5996909f3ab88ff45fdcdf083bfe816c74a84508
TRECN <rishabh1103@outlook.com>
2022-02-19 18:06:31 +0530

    commit

--- RansomNote.cpp
+++ RansomNote.cpp
@@ -2,9 +2,18 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int r=ransomNote.length();
-        int m=magazine.length()
+        int m=magazine.length();
         if(r<m){
-            for(int i=0;)
+            for(int i=0;i<m&&(i+r)<=m;i++){
+                if(ransomeNote==magazine.substr(i,r)){
+                    return true;
+                }
+            }
+
         }
+        else if(r==m&&ransomeNote==magazine){
+            return true;
+        }
+        return false
     }
 };
