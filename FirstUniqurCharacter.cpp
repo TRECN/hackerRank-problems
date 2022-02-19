6740a44d2e19009e0e92fd917caabbab77b40459
TRECN <rishabh1103@outlook.com>
2022-02-19 17:42:11 +0530

    comit

--- FirstUniqurCharacter.cpp
+++ FirstUniqurCharacter.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-                          
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<s.length();j++){
+                if(s[i]==s[j]){
+                    count++;
+                }
+            }
+            if(count==1)
+                return i;
+        }
+        return -1;    
     }
 };
