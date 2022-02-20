5dcf9b319bcf2bf3b04033d7a0f2b52d39237b81
TRECN <rishabh1103@outlook.com>
2022-02-20 23:09:51 +0530

    commit

--- GradingStudent.cpp
+++ GradingStudent.cpp
@@ -49,23 +49,7 @@ int main()
     for (size_t i = 0; i < result.size(); i++) {
         fout << result[i];
 
-        if (i != result.size() - 1) {
-            fout << "\n";
-        }
-    }
 
-    fout << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
         find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
     );
 
