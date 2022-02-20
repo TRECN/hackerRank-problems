b46838e10d0797f44d67758fd5d1db05ddfffe47
TRECN <rishabh1103@outlook.com>
2022-02-20 22:34:19 +0530

    commit

--- medianSortedArr.cpp
+++ medianSortedArr.cpp
@@ -17,7 +17,7 @@ public:
             result=a[median];
         }
         else{
-            median=(n/2);
+            median=(n/2)-1;
             
             result =(a[median]+a[median+1])/2.0;
         }
