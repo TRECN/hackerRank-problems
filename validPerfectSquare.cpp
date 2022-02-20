9ae285d9d13b75932e5eb247c9cc42fefe901cb2
TRECN <rishabh1103@outlook.com>
2022-02-20 22:22:45 +0530

    commit

--- validPerfectSquare.cpp
+++ validPerfectSquare.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        
+        long l=1;
+        long r=x;
+        long m;
+        while(l<=r){
+            m=(l+r)/2;
+            if(m*m==x){
+                return true;
+            }
+            else if(m*m<x)
+                l=m+1;
+            else
+                r=m-1;
+        }
+        return false;
     }
 };
