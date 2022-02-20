d30bec0f1299dccd1e8102378014f884c48a88d9
TRECN <rishabh1103@outlook.com>
2022-02-20 22:53:50 +0530

    commit

--- longestSubRep.cpp
+++ longestSubRep.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        string str="";
+        string result="";
+        char pre=' ';
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                char ch=str[j];
+                if(pre!=ch){
+                    str+=ch;
+                    pre=ch;
+                }
+                else{
+                    pre=' ';
+                    if(str.size()>result.size())
+                        result=str;
+                    str="";
+                }
+
+            }
+        }
+        return result.size();
     }
 };
