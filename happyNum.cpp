301744d6b4238abcdfaf04d19d88faefc8ee203f
TRECN <rishabh1103@outlook.com>
2022-02-20 23:03:22 +0530

    commit

--- happyNum.cpp
+++ happyNum.cpp
@@ -3,11 +3,20 @@ public:
     bool isHappy(int n) {
         int a=n;
         int s,r;
-        while(a>0){
-            if(a!=1){
+        while(true){
                 r=a%10;
                 s=s+(r*r);
-            }
+                a=a/10;
+                if(s==0){
+                    return false;       
+                }
+                if(s==1)
+                    return true
+                if(a==0){
+                    a=s;
+                    s=0;
+                }
         }
+        return false;
     }
 };
