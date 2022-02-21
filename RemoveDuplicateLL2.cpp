57c900cb66fc7b8173d98ad6790ae02b4328b70d
TRECN <rishabh1103@outlook.com>
2022-02-21 17:10:41 +0530

    commit

--- RemoveDuplicateLL2.cpp
+++ RemoveDuplicateLL2.cpp
@@ -12,13 +12,21 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         ListNode* a=head;
+        ListNode* temp=new ListNode(INT_MIN);
+        ListNode* result=temp;
+        temp->next=a;
         while(a!=NULL && a->next!=NULL){
             if(a->val==a->next->val){
-                a->next=a->next->next;
-            }else{
+                int b=a->val;
+                while(a->val==b)
+                    a=a->next
+            }
+            else{
+                temp->next=a;
+                temp=temp->next
                 a=a->next;
             }
         }
-        return head;
+        return result->next;
     }
 };
