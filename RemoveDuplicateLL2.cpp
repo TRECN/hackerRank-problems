212acfbd912c82ce29723951a39c8a90531e29cf
TRECN <rishabh1103@outlook.com>
2022-02-21 17:39:18 +0530

    commit

--- RemoveDuplicateLL2.cpp
+++ RemoveDuplicateLL2.cpp
@@ -15,8 +15,8 @@ public:
         ListNode* temp=new ListNode(0,a);
         ListNode* pre=temp;
         while(a!=NULL){
-            if(a->val==a->next->val &&a->next){
-                while(a->val==a->next&&a->next)
+            if(a->val==a->next->val &&a->next!=NULL){
+                while(a->val==a->next->val&&a->next!=NULL)
                     a=a->next;
             }
             else{
@@ -28,3 +28,33 @@ public:
         return temp->next;
     }
 };
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // sentinel
+        ListNode sentinel = new ListNode(0, head);
+
+        // predecessor = the last node 
+        // before the sublist of duplicates
+        ListNode pred = sentinel;
+        
+        while (head != null) {
+            // if it's a beginning of duplicates sublist 
+            // skip all duplicates
+            if (head.next != null && head.val == head.next.val) {
+                // move till the end of duplicates sublist
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+                // skip all duplicates
+                pred.next = head.next;     
+            // otherwise, move predecessor
+            } else {
+                pred = pred.next;    
+            }
+                
+            // move forward
+            head = head.next;    
+        }  
+        return sentinel.next;
+    }
+}
