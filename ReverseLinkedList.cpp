c19d071cac13a304bfabb3489babc24af1526b74
TRECN <rishabh1103@outlook.com>
2022-02-21 16:23:28 +0530

    commit

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -11,6 +11,17 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        
+        ListNode* a=head;
+        ListNode* b=NULL;
+        ListNode* c;
+        while(a){
+            c=a->next;
+            a->next=b;
+            b=a;
+            a=c;
+        }
+        head=b;
+
     }
+    return head;
 };
