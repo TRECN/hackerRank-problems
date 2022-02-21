7e74716876b1a7cf68dd818e68d48130dbb8c7ef
TRECN <rishabh1103@outlook.com>
2022-02-21 21:13:14 +0530

    commit

--- RemoveDuplicatesLL.cpp
+++ RemoveDuplicatesLL.cpp
@@ -8,3 +8,17 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* a=head;
+        while(a!=NULL && a->next!=NULL){
+            if(a->val==a->next->val){
+                a->next=a->next->next;
+            }else{
+                a=a->next;
+            }
+        }
+        return head;
+    }
+};
