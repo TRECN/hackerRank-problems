64b32061459982d90eeb4060b0d7d0ea02f0bc73
TRECN <rishabh1103@outlook.com>
2022-02-22 22:24:55 +0530

    commit

--- DefangingIP.cpp
+++ DefangingIP.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        
+        string a="";
+        for(auto i:address){
+            if(i==".")
+                a+="[.]";
+            else    
+                a+=i;
+        }
+        return a;
     }
 };
