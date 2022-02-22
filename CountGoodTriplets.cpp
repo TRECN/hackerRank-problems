55eb00560eb019b86d212c20f10f4af32a6fc14e
TRECN <rishabh1103@outlook.com>
2022-02-22 20:21:33 +0530

    commit

--- CountGoodTriplets.cpp
+++ CountGoodTriplets.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<arr.size();i++){
             for(int j=i+1;j<arr.size()-1;j++){
                 if(abs(arr[i]-arr[j])<=a){
-                    for(int k=j-1;k<arr.size();k++){
+                    for(int k=j+1;k<arr.size();k++){
                         if(abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c)
                             result++;
                     }
