5e620a759e293ccf67a7af2aa3059ae50fcc3332
TRECN <rishabh1103@outlook.com>
2022-02-22 22:20:21 +0530

    commit

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -6,3 +6,20 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* temp=head;
+        if(!head){
+            return false;
+        }
+        while(temp->next){
+            if(temp>=temp->next){
+                return true;
+            }
+            temp=temp->next;
+        }
+        return false;
+
+    }
+};
