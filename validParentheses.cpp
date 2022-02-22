48e10b26c7e36f90584a40dc1631056791808a77
TRECN <rishabh1103@outlook.com>
2022-02-22 17:33:25 +0530

    commit

--- validParentheses.cpp
+++ validParentheses.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<int>st;
+        for(auto i:s){
+            if(i=='('||i=='{'||i=='[')
+                st.push(i);
+            else{
+                if(st.empty())
+                    return false;
+                if((st.top()=="("&&i==')')||(st.top()=="{"&&i=='}')||(st.top()=="["&&i==']'))
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
     }
 };
