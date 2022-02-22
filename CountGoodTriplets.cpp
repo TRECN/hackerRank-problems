9273469f3533ef1d0cb0e8024a78dce31ac57144
TRECN <rishabh1103@outlook.com>
2022-02-22 20:16:11 +0530

    commit

--- CountGoodTriplets.cpp
+++ CountGoodTriplets.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int result=0;
         for(int i=0;i<arr.size();i++){
-            
+            for(int j=i+1;j<arr.size()-1;j++){
+                if(abs(arr[i]-arr[j])<=a){
+                    for(int k=j-1;k<n;k++){
+                        if(abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k]<=c))
+                            ans++;
+                    }
+                }
+            }
         }
+        return result;
     }
 };
