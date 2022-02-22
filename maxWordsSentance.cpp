4591dfa48227b58ad0794ea90ce711e601509421
TRECN <rishabh1103@outlook.com>
2022-02-22 20:28:53 +0530

    commit

--- maxWordsSentance.cpp
+++ maxWordsSentance.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
+        int result=0;
         for(auto i:sentences){
-            
+            int count=0;
+            for(int j=0;j<i.length();j++){
+                char ch[a]=i[j];
+                if(ch==' '){
+                    count++;
+                }
+
+            }
+            count=count+1;
+            if(result<count)
+                result=count;
         }
+        return result;
     }
 };
