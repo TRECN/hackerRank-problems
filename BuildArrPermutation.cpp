d49b3d7bb8757f5fc9b72a8d14fcc8c03d870e30
TRECN <rishabh1103@outlook.com>
2022-02-22 17:44:43 +0530

    commit

---
+++ BuildArrPermutation.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int>ar;
+        for(auto i:nums){
+            ar.push_back[nums[i]];
+        }
+        return ar;
+    }
+};
