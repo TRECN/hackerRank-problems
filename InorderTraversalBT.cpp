bf0a0de8f919e852e61ed81f1d6669a6e3a770dc
TRECN <rishabh1103@outlook.com>
2022-02-23 18:06:52 +0530

    commit

--- InorderTraversalBT.cpp
+++ InorderTraversalBT.cpp
@@ -11,7 +11,16 @@
  */
 class Solution {
 public:
+    vector<int>a;
+    void inorder(TreeNode* root){
+        if(root==NULL)
+            return;
+        inorder(root->left);
+        a.push_back(root->val);
+        inorder(root->right);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-        
+        inorder(root);
+        return a;
     }
 };
