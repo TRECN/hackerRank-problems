29d82c7e04adcd75a991a24fdf1590bca0fd2fe7
TRECN <rishabh1103@outlook.com>
2022-02-24 22:26:16 +0530

    commit

--- LengthofLastWord.cpp
+++ LengthofLastWord.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+        int count=0;
+        bool x=true;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s[i]!=' ')
+            {
+                x=false;
+                count++;
+            }
+           if(x==false && s[i]==' ')\
+           {
+               break;
+           }
+        }
+        return count;
     }
 };
