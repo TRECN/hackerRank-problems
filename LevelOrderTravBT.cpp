a8c8ebb2df40052929ac94b62529b2d1c1c3d64c
TRECN <rishabh1103@outlook.com>
2022-02-24 15:34:00 +0530

    commit

---
+++ LevelOrderTravBT.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector <int>>result;
+    void level(TreeNode* root,int lev){
+        if(root==NULL)
+            return;
+        if(lev==result.size())result.push_back({});
+        result[lev].push_back(node->val);
+        level(root->left,lev+1);
+        level(root->right,lev+1);
+
+        
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(root!=NULL)
+            level(root,0);
+        return result;
+    }
+};
