25ab23e23f8091b4be68527ee725aea19b9767fa
TRECN <rishabh1103@outlook.com>
2022-02-24 22:11:28 +0530

    commit

--- IsomorphicString.cpp
+++ IsomorphicString.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        unordered_map<char,int>m;
         
+        for(long i=0;i<s.length();i++){
+            m[s[i]]++;
+        } 
+
+        unordered_map<char,int>m1;
+        
+        for(long i=0;i<s.length();i++){
+            m1[st[i]]++;
+        }
+        if(s.length()==t.length()){
+            
+        } 
     }
 };
