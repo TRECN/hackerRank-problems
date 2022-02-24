848f49dcc68b4e739fc2f548bc6389a460857ff2
TRECN <rishabh1103@outlook.com>
2022-02-24 22:02:20 +0530

    commit

--- GoodPaiers.cpp
+++ GoodPaiers.cpp
@@ -3,7 +3,12 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int count=0;
         for (int i=0;i<nums.size();i++){
-            
+            for(int j=i+1;j<nums.size()-1;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
         }
+        return count;
     }
 };
