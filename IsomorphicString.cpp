bd7fc7b28e691413290c19bdf3c214c4b862a843
TRECN <rishabh1103@outlook.com>
2022-02-24 22:18:24 +0530

    commit

--- IsomorphicString.cpp
+++ IsomorphicString.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,int>m;
-        
-        for(long i=0;i<s.length();i++){
-            m[s[i]]++;
-        } 
-
-        unordered_map<char,int>m1;
-        
-        for(long i=0;i<s.length();i++){
-            m1[t[i]]++;
-        }
-        if(s.length()!=t.length()){
-            return false;
-        }
-        for(int j=0;j<s.length();j++){
-            if(m[s[j]]!=m1[t[j]]){
+        unordered_map<char,char> ums;
+        unordered_map<char,char> umt;
+        for(int i = 0; i < s.length(); ++i)
+        {
+            if(ums.find(s[i]) != ums.end() && ums[s[i]] != t[i])
                 return false;
-            }
+            if(umt.find(t[i]) != umt.end() && umt[t[i]] != s[i])
+                return false;
+            ums[s[i]] = t[i];
+            umt[t[i]] = s[i];
         }
-        return true; 
+        return true;
     }
 };
