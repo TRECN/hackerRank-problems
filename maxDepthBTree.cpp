094b908207f56d5629fe235d846221ee6cc9a9f2
TRECN <rishabh1103@outlook.com>
2022-02-24 15:38:30 +0530

    commit

--- maxDepthBTree.cpp
+++ maxDepthBTree.cpp
@@ -12,6 +12,10 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        
+        if(root==NULL)
+            return 0;
+        int maxL=maxDepth(root->left);
+        int maxR=maxDepth(root->right);
+        return max(maxL,maxR)+1;
     }
 };
