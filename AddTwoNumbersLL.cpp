288bb05e26695b54aa62b4acd6317fcb455227ab
TRECN <rishabh1103@outlook.com>
2022-02-25 22:06:33 +0530

    commit

--- AddTwoNumbersLL.cpp
+++ AddTwoNumbersLL.cpp
@@ -100,33 +100,3 @@ public:
                 sum+=a->val;
             if(b!=NULL)
                 sum+=b->val;
-
-            sum+=carry;
-            if(sum>=10){
-                sum=sum%10;
-                carry=1;
-            }
-            else
-                carry=0;
-
-            if(!result){
-                result= new ListNode(sum);
-                temp=result;
-            }
-            else{
-                temp->next=new ListNode(sum);
-                temp=temp->next;
-            }
-            if(a)
-                a=a->next;
-            if(b)
-                b=b->next;
-
-        }
-        if(carry)
-            temp->next=new ListNode(carry);
-        
-        return result;
-
-    }
-};
