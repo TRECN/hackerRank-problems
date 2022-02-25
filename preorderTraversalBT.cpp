34ae31edda1b9f61d108c00e8c4e9883e39a6633
TRECN <rishabh1103@outlook.com>
2022-02-25 22:04:11 +0530

    commit

--- preorderTraversalBT.cpp
+++ preorderTraversalBT.cpp
@@ -9,3 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class Solution {
+public:
+    vector<int>a;
+    void preorder(TreeNode* root){
+        if(root==NULL)
+            return;
+            a.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        preorder(root);
+        return a;
+    }
+};
