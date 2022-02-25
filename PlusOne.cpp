4eb838093b8009d0c9e79d5f28d20a82667ceaf2
TRECN <rishabh1103@outlook.com>
2022-02-25 22:40:27 +0530

    commit

--- PlusOne.cpp
+++ PlusOne.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int sum=0;
-        for(auto a:digits){
-            sum=sum*10+a;
+        int i, j = 1, carry = 0, sum = 0, first;
+        vector<int> vect;
+        i = digits.size() - 1;
+        
+        while(i != -1 || j != 0 || carry){
+            if(i != -1) first = digits[i--];
+            else first = 0;
+            
+            sum =  first + j + carry;
+            vect.push_back(sum % 10);
+            carry = sum / 10;
+            j = 0;
         }
-        sum+=1;
-        vector<int>result;
-        while(sum){
-            result.push_back(sum%10);
-            sum/=10;
-        }
-        reverse(result.begin(),result.end());
-        return result;
+        reverse(vect.begin(), vect.end());
+        return vect;
     }
 };
