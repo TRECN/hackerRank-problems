efd69a1a8a215f339204127c10586adac36fb031
TRECN <rishabh1103@outlook.com>
2022-02-25 22:02:15 +0530

    commit

--- N-aryTreePreTrav.cpp
+++ N-aryTreePreTrav.cpp
@@ -18,3 +18,18 @@ public:
 };
 */
 
+class Solution {
+public:
+    vector<int>a;
+    void pre(Node* root){
+        if(root==NULL)
+            return;
+        a.push_back(root->val);
+        for(auto i:root->children)
+            pre(i);
+    }
+    vector<int> preorder(Node* root) {
+        pre(root);
+        return a;
+    }
+};
