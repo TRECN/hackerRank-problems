b361fd78814e405f16a874d8e0ce63e0746e3b6f
TRECN <rishabh1103@outlook.com>
2022-02-25 16:27:18 +0530

    commit

--- invertBinaryTree.cpp
+++ invertBinaryTree.cpp
@@ -12,6 +12,10 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        
+        if(root==NULL)return root;
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left,root->right);
+        return root;
     }
 };
