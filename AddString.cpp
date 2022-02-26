3880cbe9e70e880c365f265820bed99ecad3a55a
TRECN <rishabh1103@outlook.com>
2022-02-26 19:20:32 +0530

    commit

--- AddString.cpp
+++ AddString.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        
+        int n;
+        int n1;
+        n=stoi(num1);
+        n1=stoi(num2);
+        n=n+n1;
+        string n2=n;
+        return n2;
+
+
     }
 };
