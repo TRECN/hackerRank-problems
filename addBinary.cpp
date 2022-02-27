2d4f465538b8f6be244528e8898a2ffab9fc7795
TRECN <rishabh1103@outlook.com>
2022-02-27 19:43:59 +0530

    commit

--- addBinary.cpp
+++ addBinary.cpp
@@ -6,3 +6,21 @@ public:
         i=a.length()-1;
         j=b.length()-1;
 
+        while(i!=-1||j!=-1||carry){
+            if(i!=-1)
+                first =int(a[i--]-48);
+            else 
+                first =0;
+            
+            if(j!=-1)
+                second = int(b[j--]-48);
+            else
+                second =0;
+            
+            sum = first + second +carry;
+            s.insert(0,to_string(sum%2));
+            carry=sum/2;
+        }
+        return s;
+    }
+};
