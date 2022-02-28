67b20eff2d8fac5afc5d9529c0d9487af15dfe11
TRECN <rishabh1103@outlook.com>
2022-02-28 19:34:40 +0530

    commit

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+        vector<vector<int>>result;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                for(int k=0;k<nums.size();k++){
+                    
+                }
+            }
+        }
     }
 };
