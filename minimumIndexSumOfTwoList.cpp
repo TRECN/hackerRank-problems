4d8237e6f958ed9bdf80875fa35fbd9427e24ebc
TRECN <rishabh1103@outlook.com>
2022-02-28 20:04:29 +0530

    commit

--- minimumIndexSumOfTwoList.cpp
+++ minimumIndexSumOfTwoList.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        unordered_map<string,int> mp;
+        unordered_map<string,int> m;
         int i = 0;
         for(auto it: list1){
-            mp[it] = i;
+            m[it] = i;
             i++;
         }
         i = 0;
         priority_queue< pair<int,string>, vector< pair<int,string> >, greater< pair<int,string> > > pq;
         for(auto it: list2){
-            if(mp.count(it)){
-                mp[it] += i;
-                pq.push({mp[it], it});
+            if(m.count(it)){
+                m[it] += i;
+                pq.push({m[it], it});
             }
             i++;
         }
