f3dd24307e31a21599dbe3b840f109736e8bae04
TRECN <rishabh1103@outlook.com>
2022-03-01 21:39:28 +0530

    commit

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -8,26 +8,26 @@ public:
         vector<vector<int>> result;        
         sort(intervals.begin(),intervals.end());        
         int s=intervals[0][0];
-        int maxi=intervals[0][1];        
+        int max=intervals[0][1];        
         for(int i=1;i<intervals.size();i++)
         {
-            if(intervals[i][0]<=maxi){
+            if(intervals[i][0]<=max){
                 
-                if(intervals[i][1]>maxi)
-                    maxi=intervals[i][1];
+                if(intervals[i][1]>max)
+                    max=intervals[i][1];
                               
             }
             else
             {      
-                result.push_back({s,maxi});
+                result.push_back({s,max});
                 
                 s=intervals[i][0];
-                maxi=intervals[i][1];
+                max=intervals[i][1];
                 
             }
             if(i==intervals.size()-1)
                 {
-                    result.push_back({s,maxi});
+                    result.push_back({s,max});
                 }
         }
         
