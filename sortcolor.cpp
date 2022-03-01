7eda3020559ac61fe8ab0417e388719c36f6ccdd
TRECN <rishabh1103@outlook.com>
2022-03-01 21:33:32 +0530

    commit

--- sortcolor.cpp
+++ sortcolor.cpp
@@ -4,22 +4,25 @@
 //         sort(nums.begin(),nums.end());
 //     }
 // };
-void sortColors(vector<int>& nums) {
-        vector<int> count(3);
+ class Solution {
+ public:
+     void sortColors(vector<int>& nums) {
+        vector<int> c(3);
         int n = nums.size();
         
         for(auto num : nums){
-            count[num]+=1;
+            c[num]+=1;
         }
         for(int i=0;i<n;i++){
-            if(count[0]>0){
-                nums[i] = 0; count[0]--;
+            if(c[0]>0){
+                nums[i] = 0; c[0]--;
             }
-            else if(count[1]>0){
-                nums[i] = 1; count[1]--;
+            else if(c[1]>0){
+                nums[i] = 1; c[1]--;
             }
             else{
-                nums[i] = 2; count[2]--;
+                nums[i] = 2; c[2]--;
             }
         }
     }
+ };
