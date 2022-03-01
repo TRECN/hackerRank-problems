88731ba904312dbd8ce5338d9957636530efbbf7
TRECN <rishabh1103@outlook.com>
2022-03-01 21:48:51 +0530

    commit

---
+++ countingBits.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+        vector <int> countBits(int n){
+            vector<int> result;
+            for(int i=0;i<n+1;i++){
+                int s=0;
+                int a=i;
+                while(a>0){
+                    s+=a%2;
+                    a/=2;
+                }
+                a
+            }
+        }
+};
