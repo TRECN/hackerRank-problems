a6dc947baf6567f4344a023933da55b2993347d0
TRECN <rishabh1103@outlook.com>
2022-03-02 22:06:41 +0530

    commit

--- spiralmatrix.cpp
+++ spiralmatrix.cpp
@@ -4,7 +4,19 @@ public:
       int c=1;
         int i=0,j=0;
         vector<vector<int>> result(n,vector<int>(n,-1));
-
+        while(c<=n*n){   
+            while(j<n&&result[i][j]==-1){ 
+                result[i][j]=c;     
+                c++;
+                j++;
+            }
+             j--;
+             i++;
+            while(i<n&&result[i][j]==-1){   
+                result[i][j]=c;     
+                c++;
+                i++;
+            }
             i--;
             j--;
              while(j>=0&&result[i][j]==-1){
