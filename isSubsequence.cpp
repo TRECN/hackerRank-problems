417874d76a14186d7d20e012086d0d321fafc7ee
TRECN <rishabh1103@outlook.com>
2022-03-02 20:49:30 +0530

    commit

--- isSubsequence.cpp
+++ isSubsequence.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        vector<int>s1;
-        for(int i=0;i<s.size();i++){
-            char c=s[i];
-            for(int j=0;j<t.size();j++){
-                char ch=t[j];
-                if(c==ch)
-                    s1.push_back(j);
-            }
+        int j=0;
+        for(int i=0;i<t.size()&&j<s.size();i++){
+            if(s[j]==t[i])
+                j++;
         }
-        int min=INT_MIN;
-        if(s1.size()==s.size()){
-            for(auto i:s1){
-                if(min<i)
-                    min=i;
-                else
-                    return false;
-            }
-            return true;
-        }
-        return false;
+
+        return (j==s.size());
         
     }
 };
