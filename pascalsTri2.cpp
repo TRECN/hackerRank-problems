4b28f11796cc025ca26a8ac3e2833f00b89ab4bb
TRECN <rishabh1103@outlook.com>
2022-03-02 20:51:16 +0530

    commit

--- pascalsTri2.cpp
+++ pascalsTri2.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>result(numRows);
+    vector<vector<int>> generate(int rowIndex) {
+        vector<vector<int>>result(rowIndex);
         result[0].push_back(1);
-        if(numRows!=1){
+        if(rowIndex!=1){
             result[1].push_back(1);
             result[1].push_back(1);
             vector<int>prv=result[1];
-            for(int i=0;i<numRows-2;i++){
+            for(int i=0;i<rowIndex-2;i++){
                 vector<int>edg;
                 edg.push_back(1);
                 for(int j=0;j<prv.size()-1;j++)
@@ -17,6 +17,6 @@ public:
                 prv=edg;
             }
         }
-        return result;
+        return result[rowIndex-1];
     }
 };
