260ac7fc59e0c516ba7f5537b9ed1a9c42743b9b
TRECN <rishabh1103@outlook.com>
2022-03-02 22:05:46 +0530

    commit

--- spiralmatrix.cpp
+++ spiralmatrix.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-      int count=1;
+      int c=1;
         int i=0,j=0;
         vector<vector<int>> result(n,vector<int>(n,-1));
-        while(count<=n*n)
-        {   
-            while(j<n&&result[i][j]==-1)
-            { result[i][j]=count;     
-                count++;
+        while(c<=n*n){   
+            while(j<n&&result[i][j]==-1){ 
+                result[i][j]=c;     
+                c++;
                 j++;
             }
              j--;
              i++;
-            while(i<n&&result[i][j]==-1)
-            {   result[i][j]=count;     
-                count++;
+            while(i<n&&result[i][j]==-1){   
+                result[i][j]=c;     
+                c++;
                 i++;
             }
             i--;
             j--;
-             while(j>=0&&result[i][j]==-1)
-            {
-                result[i][j]=count;     
-                count++;
+             while(j>=0&&result[i][j]==-1){
+                result[i][j]=c;     
+                c++;
                 j--;
             }
              i--;   
              j++;
          
-             while(i>=0&&result[i][j]==-1)
-            {
-                result[i][j]=count;     
-                count++;
+             while(i>=0&&result[i][j]==-1){
+                result[i][j]=c;     
+                c++;
                 i--;
             }
             i++;
