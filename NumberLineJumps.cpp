091621d6f7c4339dd85f05140808f733549a7e9f
TRECN <rishabh1103@outlook.com>
2022-03-02 22:07:22 +0530

    commit

--- NumberLineJumps.cpp
+++ NumberLineJumps.cpp
@@ -17,7 +17,13 @@ vector<string> split(const string &);
  *  4. INTEGER v2
  */
 
-
+string kangaroo(int x1, int v1, int x2, int v2) {
+    
+    if((v1<=v2)||((x2-x1)%(v2-v1))){
+        return "NO";
+    }
+    return "YES";
+}
 
 int main()
 {
