c76b1a7787ebe3af99db896059a3206a55ff14d9
TRECN <rishabh1103@outlook.com>
2022-03-02 22:09:24 +0530

    commit

--- digonalDiff.cpp
+++ digonalDiff.cpp
@@ -111,31 +111,6 @@ int diagonalDifference(vector<vector<int>> arr) {
     return n;
 }
 
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    vector<vector<int>> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        arr[i].resize(n);
-
-        string arr_row_temp_temp;
-        getline(cin, arr_row_temp_temp);
-
-        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
-
-        for (int j = 0; j < n; j++) {
-            int arr_row_item = stoi(arr_row_temp[j]);
-
-            arr[i][j] = arr_row_item;
-        }
-    }
 
     int result = diagonalDifference(arr);
 
