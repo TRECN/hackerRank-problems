9bc4846df9c39a6b0065a0530cf40e64384dfdeb
TRECN <rishabh1103@outlook.com>
2022-03-03 20:31:02 +0530

    commmit

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -8,3 +8,20 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* a=head;
+        ListNode* b=NULL;
+        ListNode* c;
+        while(a){
+            c=a->next;
+            a->next=b;
+            b=a;
+            a=c;
+        }
+        head=b;
+        return head;
+    }
+    
+};
