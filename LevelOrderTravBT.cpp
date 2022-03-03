d3471759fd4944688d9a07c0c0876dc55d71571a
TRECN <rishabh1103@outlook.com>
2022-03-03 20:32:49 +0530

    commit

--- LevelOrderTravBT.cpp
+++ LevelOrderTravBT.cpp
@@ -9,22 +9,3 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    vector<vector <int>>result;
-    void level(TreeNode* root,int lev){
-        if(root==NULL)
-            return;
-        if(lev==result.size())result.push_back({});
-        result[lev].push_back(root->val);
-        level(root->left,lev+1);
-        level(root->right,lev+1);
-
-        
-    }
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root!=NULL)
-            level(root,0);
-        return result;
-    }
-};
