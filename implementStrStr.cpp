03a4bf2b8ce8735d051e4a4490f80bbca439b82c
TRECN <rishabh1103@outlook.com>
2022-03-03 20:27:38 +0530

    commit

--- implementStrStr.cpp
+++ implementStrStr.cpp
@@ -38,4 +38,19 @@ public:
         int n = haystack.length();
         int m = needle.length();
         if( m > n) return -1;
-
+        string val = "";
+        for(int i = 0; i < m; i++){
+            val += haystack[i];
+        }
+        int i;
+        for(i = m; i < n; i++){
+            if(val == needle){
+                return i-m;
+            }
+            val.erase(0,1);
+            val += haystack[i];
+        }
+        if(val == needle) return i-m;
+        return -1;
+    }
+};
