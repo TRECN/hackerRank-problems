072725e743b40cd913d2037c34f2dbc4cafc1585
TRECN <rishabh1103@outlook.com>
2022-03-03 20:27:58 +0530

    commit

--- implementStrStr.cpp
+++ implementStrStr.cpp
@@ -1,35 +1,3 @@
-// class Solution {
-// public:
-//     int strStr(string haystack, string needle) {
-//         int j=0;
-//         int s=-1;
-//         bool t=false;
-//         for(int i=0;i<haystack.size()&&j<needle.size();i++){
-//             if(haystack[i]==needle[j]){
-//                 if(j==0)
-//                     s=i;
-//                 j++;
-//                 t=true;
-//             }   
-//             else{
-//                 t=false;
-//                 s=-1;
-//                 j=0;
-//             }
-//         }
-//         if(needle.size()>haystack.size())
-//             return -1;
-//         if(needle.size()==0)
-//             return 0;
-//         else{
-//             if(t)
-//                 return s;
-//             else
-//                 return -1;
-//         }
-//         return -1;
-//     }
-// };
 
 class Solution {
 public:
