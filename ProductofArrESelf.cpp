3edd216d9fc832544d6814011cecf72189223427
TRECN <rishabh1103@outlook.com>
2022-03-04 20:39:21 +0530

    commit

--- ProductofArrESelf.cpp
+++ ProductofArrESelf.cpp
@@ -6,6 +6,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(j!=i)
+                    result[i]=1;
                     result[i]*=nums[j];
             }
         }
