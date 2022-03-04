d6bf46b72289fad51a4c3a146d65cb1f9f49d339
TRECN <rishabh1103@outlook.com>
2022-03-04 20:40:11 +0530

    commit

--- ProductofArrESelf.cpp
+++ ProductofArrESelf.cpp
@@ -4,9 +4,9 @@ public:
         int n=nums.size();
         vector<int>result(n);
         for(int i=0;i<n;i++){
+            result[i]=1;
             for(int j=0;j<n;j++){
                 if(j!=i)
-                    result[i]=1;
                     result[i]*=nums[j];
             }
         }
