35179b2771143ab6ec687e09cd93cdf5f3df6c63
TRECN <rishabh1103@outlook.com>
2022-03-04 20:38:33 +0530

    commit

--- ProductofArrESelf.cpp
+++ ProductofArrESelf.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums[size];
+        int n=nums.size();
         vector<int>result(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
