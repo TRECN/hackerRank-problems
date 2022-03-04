74072330b4e343889a121076bb8b56bc253b9e55
TRECN <rishabh1103@outlook.com>
2022-03-04 20:55:56 +0530

    commit

--- subarSumEqualsK.cpp
+++ subarSumEqualsK.cpp
@@ -1,17 +1,17 @@
 class Solution {
-public:
-int subarraySum(vector& nums, int k) {
-int size = nums.size();
-int sum=0, ans=0;
-unordered_map<int,int> map;
-map[0] = 1;
+    public:
+        int subarraySum(vector& nums, int k) {
+            int size = nums.size();
+            int sum=0, ans=0;
+            unordered_map<int,int> map;
+            map[0] = 1;
 
-    for(int i=0; i<size; i++){
-        sum += nums[i];
-        if(map[sum-k]) 
-            ans+=map[sum-k];
-        map[sum]++;
-    }
-    return ans;
-}
+            for(int i=0; i<size; i++){
+                sum += nums[i];
+                if(map[sum-k]) 
+                    ans+=map[sum-k];
+                map[sum]++;
+            }
+            return ans;
+        }   
 };
