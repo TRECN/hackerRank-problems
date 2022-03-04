65506317559ada7d692b792006665db1e6d1c407
TRECN <rishabh1103@outlook.com>
2022-03-04 21:15:38 +0530

    COMMIT

--- champagneTower.cpp
+++ champagneTower.cpp
@@ -5,13 +5,3 @@ double champagneTower(int poured, int query_row, int query_glass) {
         result[0][0] = poured;
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j <= i; j++) {
-                if (result[i][j] >= 1) {
-                    result[i + 1][j]     += (result[i][j] - 1) / 2.0;
-                    result[i + 1][j + 1] += (result[i][j] - 1) / 2.0;
-                    result[i][j] = 1;
-                }
-            }
-        }
-        return result[query_row][query_glass];
-    }
-};
