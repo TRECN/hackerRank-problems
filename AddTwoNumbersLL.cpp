d2be73c58f80709e30907adbe6f495297dae862e
TRECN <rishabh1103@outlook.com>
2022-03-07 18:40:19 +0530

    commmit

--- AddTwoNumbersLL.cpp
+++ AddTwoNumbersLL.cpp
@@ -88,31 +88,6 @@
 class Solution {
 public:
 
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* a=l1;
-        ListNode* b=l2;
-        ListNode* result=NULL;
-        ListNode* temp=result;
-        int carry=0;
-        while(a!=NULL||b!=NULL){
-            int sum=0;
-            if(a!=NULL)
-                sum+=a->val;
-            if(b!=NULL)
-                sum+=b->val;
-
-            sum+=carry;
-            if(sum>=10){
-                sum=sum%10;
-                carry=1;
-            }
-            else
-                carry=0;
-
-            if(!result){
-                result= new ListNode(sum);
-                temp=result;
-            }
             else{
                 temp->next=new ListNode(sum);
                 temp=temp->next;
