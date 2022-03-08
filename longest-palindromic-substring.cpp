99e4653c159fc482c225be4bf6cd681f2cfa46bf
TRECN <rishabh1103@outlook.com>
2022-03-08 19:13:37 +0530

    commit

--- longest-palindromic-substring.cpp
+++ longest-palindromic-substring.cpp
@@ -24,15 +24,7 @@ public:
             l = i-1;
             r = i+1;
             
-            while(l>=0 && r<s.length() && s[l] == s[r]){
-                
-                if((r-l+1) > result){
-                    result = r-l+1;
-                    max = l;
-                }
-                l--;
-                r++;
-            }
+            
         }
         return s.substr(max, result);
     }
