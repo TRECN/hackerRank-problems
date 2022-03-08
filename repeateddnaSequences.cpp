a5b3b813f6ac2371f12f6ebd748f314192e9139c
TRECN <rishabh1103@outlook.com>
2022-03-08 18:31:23 +0530

    commit

--- repeateddnaSequences.cpp
+++ repeateddnaSequences.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        
+        int k=10;
+        int l=s.size();
+        int i=0;
+        int j=0;
+        string t="";
+        vector<string>result;
+        unordered_map<string,int>m;
+        while(j<l){
+            t=t+s[j];
+            
+        }
+
     }
 };
