c6bfcf92366d9786c6e1ef6bdbe040ec47120b69
TRECN <rishabh1103@outlook.com>
2022-03-09 20:21:08 +0530

    commit

--- linkedListcycle2.cpp
+++ linkedListcycle2.cpp
@@ -3,24 +3,24 @@ public:
     ListNode *detectCycle(ListNode *head) {
         if(!head) return NULL;
         
-        ListNode *slow=head,*fast=head;
+        ListNode *s=head,*f=head;
         
         do{
-            slow = slow->next;
-            if(!slow) break;
+            s = s->next;
+            if(!s) break;
             
-            fast= fast->next;
-            if(!fast) break;
-            fast = fast->next;
-            if(!fast) break;            
+            f= f->next;
+            if(!f) break;
+            f = f->next;
+            if(!f) break;            
             
-        }while(slow!=fast);
-        if(!slow or !fast)
+        }while(s!=f);
+        if(!s or !f)
             return NULL;
         
-        fast = head;
-        while(fast !=slow)
-            fast=fast->next, slow=slow->next;
-        return fast;
+        f = head;
+        while(f !=s)
+            f=f->next, s=s->next;
+        return f;
     }
 };
