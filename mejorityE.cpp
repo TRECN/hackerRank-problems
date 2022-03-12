3465c2b3beb1bb474e504c0b05c7148a349f4e47
TRECN <rishabh1103@outlook.com>
2022-03-12 20:55:54 +0530

    commit

--- mejorityE.cpp
+++ mejorityE.cpp
@@ -3,7 +3,8 @@ public:
     int majorityElement(vector<int>& nums) {
         int count = 0, m;
         for(int a : nums){
-            if(count==0)m=a;
+            if(count==0)
+                m=a;
             if(m==a)
                 count++;
             else
