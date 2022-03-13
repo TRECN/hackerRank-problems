e93d99267c79b3cabe6b32b2b2fdff532c1e81cd
TRECN <rishabh1103@outlook.com>
2022-03-13 20:43:47 +0530

    commit

---
+++ designHashMap.cpp
@@ -0,0 +1,52 @@
+class MyHashMap {
+public:
+    MyHashMap() : map_(BUCKET_SIZE) {}
+    
+    void put(int key, int value) {
+        auto& bucket = map_[get_hash(key)];
+        for (auto& [key_, val_] : bucket) {
+            if (key_ == key) {
+                val_ = value;
+                return;
+            };
+        }
+        bucket.emplace_back(key, value);
+    }
+    
+    int get(int key) {
+        auto& bucket = map_[get_hash(key)];
+        for (auto& [key_, val_] : bucket) {
+            if (key_ == key) return val_;
+        }
+        return -1;
+        
+    }
+    
+    void remove(int key) {
+        auto& bucket = map_[get_hash(key)];
+        for (auto it = bucket.begin(); it != bucket.end(); ++it) {
+            if (it->first == key) {
+                bucket.erase(it);
+                return;
+            }
+        }
+        
+    }
+    
+    int get_hash(int key) {
+        return static_cast<int>( static_cast<size_t>(key) * HASH_CODE % BUCKET_SIZE );
+    }
+    
+private:
+    vector<vector<pair<int,int>>> map_;
+    static const int HASH_CODE = 9001; 
+    static const size_t BUCKET_SIZE = 42; 
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
