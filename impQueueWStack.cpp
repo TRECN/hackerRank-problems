21b62d915ce495d0bc6cb11ff460cebcf8209e57
TRECN <rishabh1103@outlook.com>
2022-03-13 20:37:28 +0530

    commit

--- impQueueWStack.cpp
+++ impQueueWStack.cpp
@@ -9,27 +9,6 @@ public:
     void push(int x) {
         input.push(x);
     }
-    
-    int pop() {
-        if(input.size() == 0 && output.size() == 0)
-        {
-            return -1;
-        }
-        
-        if(output.size() == 0)
-        {
-            while(input.size()!= 0)
-            {
-                output.push(input.top());
-                input.pop();
-            }
-        }
-        
-        int x = output.top();
-        output.pop();
-        return x;
-    }
-    
     int peek() {
         if(input.size() == 0 && output.size() == 0)
         {
