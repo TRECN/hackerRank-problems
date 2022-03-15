7589f843e76a717e05586d3379e24f838424fa44
TRECN <rishabh1103@outlook.com>
2022-03-15 19:54:26 +0530

    commit

---
+++ FirstMissingPositive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int result=1;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(result<nums[i])
+                break;
+            else if(result==nums[i])
+                result++;
+
+        }
+        
+        return result;
+    }
+};
