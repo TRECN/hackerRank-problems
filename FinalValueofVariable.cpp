ebde00fe5f78981e648b17541911e3cee58a8b8d
TRECN <rishabh1103@outlook.com>
2022-03-16 18:20:02 +0530

    commit

---
+++ FinalValueofVariable.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        for(auto i:operations){
+            if(i=="X++" || i=="++X"){
+                x++;
+            }
+            else
+                x--;
+        }
+        return x;
+    }
+
+};
