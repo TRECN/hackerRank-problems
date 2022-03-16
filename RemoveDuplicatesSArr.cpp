1222235aa9011f5e42d24b5b40ddfcded55dc4bb
TRECN <rishabh1103@outlook.com>
2022-03-16 18:16:18 +0530

    commit

--- RemoveDuplicatesSArr.cpp
+++ RemoveDuplicatesSArr.cpp
@@ -4,7 +4,14 @@ public:
         int i=1,j=1;
         if(nums.size()!=1){
             
-           
+            while(i<nums.size()){
+                if(nums[i-1]!=nums[i]){
+                    nums[j]=nums[i];
+                    j++;
+                }
+                i++;
+                
+            }
         }
         else    
             return 1;
