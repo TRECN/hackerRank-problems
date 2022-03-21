de88df8492dc5b69262875e390a12ee968f56fb9
TRECN <rishabh1103@outlook.com>
2022-03-21 15:07:20 +0530

    commit

--- firstBadVersion.cpp
+++ firstBadVersion.cpp
@@ -4,6 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        
+        int n=nums.size();
+            int l=0;
+            int r=n-1;
+            while(l<=r){
+                int m=(l+r)/2;
+                if(!isBadVersion(m))
+                    l=m+1;
+                else
+                    r=m-1;
+            }
+            return r+1;
     }
 };
