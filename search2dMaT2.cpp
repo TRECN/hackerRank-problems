788dc098a22a68328710393889026d79397b246b
TRECN <rishabh1103@outlook.com>
2022-03-21 15:13:49 +0530

    commit

---
+++ search2dMaT2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        vector<int>Mtar;
+        
+        for(int i=0;i<matrix.size();i++){
+            Mtar=matrix[i];
+            int l=0;
+            int r=Mtar.size()-1;
+            while(l<=r){
+                int m=l+(r-l)/2;
+
+                if(Mtar[m]==target)
+                    return true;
+
+                if(Mtar[m]<target)
+                    l=m+1;
+                else    
+                    r=m-1;
+            }
+        }
+        return false;
+        
+    }
+};
