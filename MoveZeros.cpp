0a76a8024a048877bfbe3243f1aeb191ef03ec0b
TRECN <rishabh1103@outlook.com>
2022-03-23 19:52:19 +0530

    commit

--- MoveZeros.cpp
+++ MoveZeros.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
+        int n=nums.size();
+        vector<int>x;
+        for(int i=0;i<n;i++){
+            if(nums)
+        }
     }
 };
