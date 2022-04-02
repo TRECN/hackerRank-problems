d7ab0fae64b84b0cc418942942c406ec1f3f7256
TRECN <rishabh1103@outlook.com>
2022-04-02 22:06:20 +0530

    commit

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_map<int,int>m;
-        int result;
-        for(auto a:nums){
-            m[a]++;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(!m[i].second){
-                result=i;
+        int result=0;
+        sort(nums.begin(),nums.end());
+        if(nums[nums.size()-1]==nums.size()){
+            for(int i=0;i<nums.size()-1;i++){
+            int a=nums[i+1]-nums[i];
+            if(a==1)
+                continue;
+            else if(a>1){
+                result=1+nums[i];
+                break;
             }
+        }
+        }
+        else{
+            return nums.size();
         }
         return result;
     }
