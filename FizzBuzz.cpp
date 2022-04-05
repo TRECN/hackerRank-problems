2244eb6e8ee94f7cc6cd39964e46ea00bef68ede
TRECN <rishabh1103@outlook.com>
2022-04-05 21:28:01 +0530

    commit

---
+++ FizzBuzz.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>result;
+        for(int i=0;i<n;i++){
+            int k=i+1;
+            if(k%5==0&&k%3==0)
+                result.push_back("FizzBuzz");
+            else if(k%3==0)
+                result.push_back("Fizz");
+            else if(k%5==0)
+                result.push_back("Buzz");
+            else
+                {
+                    string s=""+k;
+                    result.push_back(s);
+                }
+            
+                
+        }
+    }
+};
