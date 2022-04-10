894b023cc643f3f2a90c16c262df93bfbf74ed2d
TRECN <rishabh1103@outlook.com>
2022-04-10 20:43:31 +0530

    commit

--- BaseballGame.cpp
+++ BaseballGame.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
-        
+        stack<int>a;
+        for(auto a:ops){
+            if(a=="C")
+                a.pop;
+            else if(a=="D")
+                a.push(a.top*2);
+            else if(a=="+"){
+                int t=a.top();
+                a.pop();
+                t=t+a.top();
+                a.push(t-a.top());
+                a.push(t);
+            }
+            else{
+                a.push(stoi(a));
+            }
+            
+        }
+        auto result;
+        for(auto i:a){
+            result+=i;
+        }
+
+        return i;
     }
 };
